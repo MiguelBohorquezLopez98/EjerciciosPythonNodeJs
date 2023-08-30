81dab921d76b6795bce6b8f729c3aefe68c18e1c
Miguel Bohórquez <dlsg1009@gmail.com>
2023-08-30 14:37:55 -0500

    Archivo main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from conexion import *
+from contacto import *
 
 con = conectar()
 crear_tabla(con)
@@ -13,6 +14,21 @@ def iniciar():
     print('\t4. Modificar un contacto')
     print('\t5. Eliminar un contacto')
     print('\t1. Salir de la aplicación')
-    input('Escoja una opción: ')
+    opcion = input('Escoja una opción: ')
+    if opcion == '1':
+        nuevo_contacto()
+    # elif opcion == '2':
+    #     mostrar_contactos()
+    
+
+    def nuevo_contacto():
+        nombre = input('Ingrese el nombre: ')
+        apellidos = input('Ingrese el apellido: ')
+        empresa = input('Ingrese la empresa: ')
+        telefono = input('Ingrese el telefono: ')
+        email = input('Ingrese el email: ')
+        direccion = input('Ingrese la direccion: ')
+        respuesta = registrar(nombre, apellidos, empresa, telefono, email, direccion)
+        print(respuesta)
 
 iniciar()
